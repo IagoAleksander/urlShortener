843f66ec8ffb54ae8e2afd5387109734e38c1d76
Iago Aleksander <iago.zanardo@monitoratec.com.br>
2021-06-05 01:07:04 -0300

    Added color, fonts and other assets as resources

---
+++ url_shortener/lib/resources/colors.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class ColorsRes {
+  ///Primary Colors
+  static const Color primaryCyan = Color(0xFF2ACFCF);
+  static const Color primaryDarkViolet = Color(0xFF3B3054);
+
+  ///Secondary Colors
+  static const Color secondaryRed = Color(0xFFF46262);
+
+  ///Neutral Colors
+  static const Color neutralLightGray = Color(0xFFBFBFBF);
+  static const Color neutralGray = Color(0xFF9E9AA7);
+  static const Color neutralGrayishViolet = Color(0xFF35323E);
+  static const Color neutralVeryDarkViolet = Color(0xFF232127);
+
+  ///Background Colors
+  static const Color backgroundWhite = Color(0xFFFFFFFF);
+  static const Color backgroundOffWhite = Color(0xFFF0F1F6);
+}
